b6e0e5ae9b9416f7c89747a2ea91c30f382f0e5d
Silvia Canelón <49913337+spcanelon@users.noreply.github.com>
2024-11-18 21:31:21 -0500

    ext: update academicons

--- _extensions/schochastics/academicons/academicons.lua
+++ _extensions/schochastics/academicons/academicons.lua
@@ -6,7 +6,7 @@ local function ensureHtmlDeps()
   quarto.doc.addHtmlDependency({
     name = "academicons",
     version = "1.9.2",
-    stylesheets = {"assets/css/all.css", "assets/css/size.css"}
+    stylesheets = { "assets/css/all.css", "assets/css/size.css" }
   })
 end
 
@@ -31,7 +31,6 @@ end
 
 return {
   ["ai"] = function(args, kwargs)
-
     local group = ""
     local icon = pandoc.utils.stringify(args[1])
     if #args > 1 then
@@ -40,26 +39,31 @@ return {
     end
 
     local size = isValidSize(pandoc.utils.stringify(kwargs["size"]))
-    
     local color = pandoc.utils.stringify(kwargs["color"])
     if not isEmpty(color) then
-      color = " style=\"color:" .. color  .. "\""
-    else
-      color = " style=\"color:" .. "black"  .. "\""
+      color = " style=\"color:" .. color .. "\""
+      --else
+      -- color = " style=\"color:" .. "black"  .. "\""
     end
-    
+
     local title = pandoc.utils.stringify(kwargs["title"])
     if not isEmpty(title) then
-      title = " title=\"" .. title  .. "\""
+      title = " title=\"" .. title .. "\""
     end
 
     -- detect html (excluding epub)
     if quarto.doc.isFormat("html:js") then
       ensureHtmlDeps()
       if isEmpty(size) then
+        local csize = pandoc.utils.stringify(kwargs["size"])
+        if (isEmpty(csize)) then
+          csize = ""
+        else
+          csize = " style=\"font-size:" .. csize .. "\""
+        end
         return pandoc.RawInline(
           'html',
-          "<i class=\"ai " .. group .. " ai-" .. icon .. "\"" .. title .. color .. "></i>"
+          "<i class=\"ai " .. group .. " ai-" .. icon .. "\"" .. title .. color .. csize .. "></i>"
         )
       else
         return pandoc.RawInline(
@@ -67,13 +71,12 @@ return {
           "<i class=\"ai " .. group .. " ai-" .. icon .. size .. "\"" .. title .. color .. "></i>"
         )
       end
-    -- detect pdf / beamer / latex / etc
-    -- elseif quarto.doc.isFormat("pdf") then
-    --   ensureLatexDeps()
-    --   return pandoc.RawInline('tex', "\\aiIcon{" .. icon .. "}") 
+      -- detect pdf / beamer / latex / etc
+      -- elseif quarto.doc.isFormat("pdf") then
+      --   ensureLatexDeps()
+      --   return pandoc.RawInline('tex', "\\aiIcon{" .. icon .. "}")
     else
       return pandoc.Null()
     end
-
   end
 }
