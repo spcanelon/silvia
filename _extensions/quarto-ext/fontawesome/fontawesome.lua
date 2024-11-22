c9d1c653afc27e5dc16aca6998c5016de677c9c5
Silvia Canelón <49913337+spcanelon@users.noreply.github.com>
2024-11-22 17:01:21 -0500

    render site

--- _extensions/quarto-ext/fontawesome/fontawesome.lua
+++ _extensions/quarto-ext/fontawesome/fontawesome.lua
@@ -5,8 +5,8 @@ end
 local function ensureHtmlDeps()
   quarto.doc.add_html_dependency({
     name = 'fontawesome6',
-    version = '0.1.0',
-    stylesheets = {'assets/css/all.css', 'assets/css/latex-fontsize.css'}
+    version = '1.2.0',
+    stylesheets = {'assets/css/all.min.css', 'assets/css/latex-fontsize.css'}
   })
 end
 
